6e16010edf80a809e38af372520e2702b681d8f7
Yashu027 <yashwanthkumar.korimerla@my.jcu.edu.au>
2023-04-15 16:06:18 +0800

    Add website header

---
+++ home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <!-- Swiper CSS link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
+
+
+    <!-- cdnjs font awesome link. -->
+    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Custom CSS file link. -->
+    <link rel = "stylesheet" href = "css/style.css">
+
+</head>
+<body>
+  
+<!-- Header Start. -->
+
+<section class="header">
+
+    <a href = "home.php" class = "logo">Holiday-planner</a>
+
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="packages.php">packages</a>
+        <a href="book.php">book</a>
+        <a href="about us.php">about us</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- Header End. -->
+
+<!-- Swiper JS link. -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+<!-- Custome JS file link. -->
+<script src = "js/script.js"></script>
+
+</body>
+</html>
